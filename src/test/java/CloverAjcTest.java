23cf7853ede6c1e35a303ae4bae38dc6662c5894
Marek Parfianowicz <mparfianowicz@atlassian.com>
2015-03-05 14:18:48 +0100

    SHPXXX-5: created JavaExecutor using Apache's DefaultExecutor to launch compiled code; running compiled classes; more options for HTML report generation;

--- src/test/java/CloverAjcTest.java
+++ src/test/java/CloverAjcTest.java
@@ -15,8 +15,10 @@ import static org.junit.Assert.assertTrue;
 public class CloverAjcTest {
 
     public static final String AJC_EXAMPLE_DIR = "src/test/resources/ajc-example";
-    public static final String CLOVER_DB_PATH = AJC_EXAMPLE_DIR + "/clover/clover.db";
-    public static final String CLOVER_REPORT_PATH = AJC_EXAMPLE_DIR + "/clover";
+    public static final String CLOVER_DB_PATH = ".clover/clover.db"; // AJC_EXAMPLE_DIR + "/clover/clover.db";
+    public static final String CLOVER_REPORT_PATH = AJC_EXAMPLE_DIR + "/target/clover";
+    public static final String SOURCE_DIR = AJC_EXAMPLE_DIR + "/src";
+    public static final String TARGET_CLASSES_DIR = AJC_EXAMPLE_DIR + "/target/classes";
 
     @Test
     public void testAjcExampleWithClover() {
@@ -25,19 +27,33 @@ public class CloverAjcTest {
                 // Clover stuff
                 // TODO "-i", CLOVER_DB_PATH,
                 // AJC stuff
-                "-sourceroots", AJC_EXAMPLE_DIR,
+                "-sourceroots", SOURCE_DIR,
+                "-d", TARGET_CLASSES_DIR,
                 "-noExit"
         };
         CloverAjc.main(compilerArgs);
 
-        assertTrue(new File(AJC_EXAMPLE_DIR, "CloneablePoint.class").exists());
+        assertTrue(new File(TARGET_CLASSES_DIR, "introduction/CloneablePoint.class").exists());
         assertTrue(new File(CLOVER_DB_PATH).exists());
+
+        // run classes
+        String M2 = "C:\\Users\\Marek\\.m2\\repository\\";
+        String CLASSPATH = M2 + "org\\aspectj\\aspectjrt\\1.8.4\\aspectjrt-1.8.4.jar;"
+                + M2 + "com\\atlassian\\clover\\clover\\4.0.2\\clover-4.0.2.jar;"
+                + TARGET_CLASSES_DIR;
+        JavaExecutor.launchJava("-cp", CLASSPATH, "introduction.CloneablePoint");
+        JavaExecutor.launchJava("-cp", CLASSPATH, "introduction.ComparablePoint");
+        JavaExecutor.launchJava("-cp", CLASSPATH, "introduction.HashablePoint");
+        JavaExecutor.launchJava("-cp", CLASSPATH, "introduction.Point");
 
         // generate html report
         CloverStartup.loadLicense(Logger.getInstance());
         String [] reporterArgs = {
                 "-i", CLOVER_DB_PATH,
-                "-o", CLOVER_REPORT_PATH
+                "-o", CLOVER_REPORT_PATH,
+                "-a",
+                "-e",
+                "-d"
         };
         int result = HtmlReporter.runReport(reporterArgs);
         assertEquals(0, result);
