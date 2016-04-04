b6deb175c7850acf036440abedcba702af99a611
Marek Parfianowicz <mparfianowicz@atlassian.com>
2016-04-04 11:38:13 +0200

    CLOV-1771: clean up JavaExecutor code

--- src/test/java/JavaExecutor.java
+++ src/test/java/JavaExecutor.java
@@ -1,30 +1,25 @@
-import clover.com.google.common.collect.Lists;
 import clover.org.apache.commons.lang.StringUtils;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class JavaExecutor {
 
-    public static void launchJava(String... params) {
-        List<String> args = Lists.newArrayList(
-                System.getenv("JAVA_HOME") + File.separator + "bin" + File.separator + "java",
-                " ");
-        args.addAll(Arrays.asList(params));
-        launchCmd(args.toArray(new String[0]));
+    public static void launchJava(final String... params) {
+        final String javaExecutable = System.getenv("JAVA_HOME") + File.separator + "bin" + File.separator + "java";
+        launchCommand(javaExecutable, params);
     }
 
-    public static void launchCmd(String[] cmd) {
+    private static void launchCommand(final String executable, final String... arguments) {
         System.out.println("Executing:");
-        System.out.println(StringUtils.join(cmd, " "));
+        System.out.println(executable + " " + StringUtils.join(arguments, " "));
 
         try {
-            CommandLine commandLine = CommandLine.parse(StringUtils.join(cmd, " "));
-            DefaultExecutor executor = new DefaultExecutor();
+            final CommandLine commandLine = new CommandLine(executable);
+            commandLine.addArguments(arguments);
+            final DefaultExecutor executor = new DefaultExecutor();
             executor.execute(commandLine);
         } catch (IOException e) {
             e.printStackTrace();
